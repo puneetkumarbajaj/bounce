7f9961c4b97cb399eda803b301b5718ddbf48743
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-11 00:07:36 -0400

    modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,206 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bounce/flutter_bounce.dart';
-void main() {
-  runApp(const MyApp());
+
+void main() => runApp(Bounce());
+
+class Bounce extends StatefulWidget {
+  @override
+  _Bounce createState() => _Bounce();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _Bounce extends State<Bounce>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _ballPosition;
 
-  // This widget is the root of your application.
+  double _ballSpeed = 1.0;
+  Color _ballColor = Colors.red;
+  bool _sliderStatus = true;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
     );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
+    _ballPosition = Tween<double>(begin: -1.5, end: 0.5).animate(_controller);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void startBouncing() {
     setState(() {
-      _counter++;
+      _controller.forward();
+      _sliderStatus = false;
     });
   }
 
+  void pauseBouncing() {
+    setState(() {
+      _controller.stop();
+      _sliderStatus = true;
+    });
+  }
+
+  void updateAnimationSpeed(double value) {
+    _ballSpeed = value;
+    _controller.duration = Duration(seconds: 6-value.toInt());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bouncy Bounce'),
+          backgroundColor: Colors.black,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Change the ball's color",
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _ballColor = Colors.red;
+                      });
+                    },
+                    child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        )),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _ballColor = Colors.blue;
+                      });
+                    },
+                    child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        )),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _ballColor = Colors.green;
+                      });
+                    },
+                    child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        )),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return Container(
+                    width: 300,
+                    height: 300,
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 5),
+                      curve: Curves.easeInOut,
+                      child: Align(
+                        alignment: Alignment(0, 0.5 + _ballPosition.value),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: _ballColor,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: startBouncing,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey),
+                    child: const Text('Bouncy Bounce'),
+                  ),
+                  ElevatedButton(
+                    onPressed: pauseBouncing,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey),
+                    child: const Text('No more Bounce :/'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Speedy Speed: ${_ballSpeed.toInt()}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Slider(
+                value: _ballSpeed,
+                onChanged: !_sliderStatus
+                    ? null
+                    : (value) {
+                        setState(() {
+                          _ballSpeed = value;
+                        });
+                        updateAnimationSpeed(value);
+                      },
+                min: 1.0,
+                max: 5.0,
+                activeColor: Colors.grey,
+                inactiveColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
